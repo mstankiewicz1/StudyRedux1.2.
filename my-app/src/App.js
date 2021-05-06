b8df7dc8ea74d790c72938005f1ac2562fa688f0
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2021-05-06 17:17:43 +0200

    finish build app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Login from './Components/Login';
+import Logout from './Components/Logout';
+import { selectUser } from './features/userSlice';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const user = useSelector(selectUser);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? <Logout/> : <Login/>}
     </div>
   );
 }
